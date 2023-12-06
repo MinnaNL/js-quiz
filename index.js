8f2ca7eb0f8527a2313b6b3d79f4eed35fbe8ef0
MinnaNL <144107523+MinnaNL@users.noreply.github.com>
2023-12-06 20:04:20 +0100

    uppdaterar

--- index.js
+++ index.js
@@ -1,65 +1,32 @@
-//JS
-//frågor!
-  
-    let questions = [
-      {
-        question: "Daenerys Targaryen is also known as the Mother of Dragons",
-        answer: true,
-      },
-      {
-        question: "Jon Snow knows all of the things",
-        answer: false,
-      },
-      {
-        question: "There are only six episodes out of 73 that don't feature any deaths",
-        answer: true,
-      },
-      {
-        question: "Ned Stark is Jon Snow's father",
-        answer: false,
-      },
-      {
-        question: "The children of the forrest created the white walkers",
-        answer: true,
-      },
-      {
-        question: "Tyrion Lannister drinks and knows things",
-        answer: true,
-      },
-      {
-        question: "The Red Wedding is not based on a real event",
-        answer: false,
-      },
-      {
-        question: "Bran Stark is known as the Three-Eyed Raven",
-        answer: true,
-      },
-      {
-        question: "Joffrey Baratheon was a beloved king",
-        answer: false,
-      },
-      {
-        question: "Winter is coming",
-        answer: true,
-      },
-    ];
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Quiz med Javascripts</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="index.js" defer></script>
+    </head>
+    <body>
+            <main>
+                <h1> A Quiz of Fire and Ice*</h1>
+                    <div class="quiz-container">
+                        <form id="quiz-form">
+                            <fieldset>
+                                <legend>Questions</legend>
+                                <!-- Frågorna läggs in här-->
+                            </fieldset>
 
-    // if (userAnswer) {
-      //  let answerValue = userAnswer.value === "true";
-      //  if (answerValue === question.answer) {
-      //    score++;
-      //  }
-  
-    // Skapar html för frågorna
-    questions.forEach((question, index) => {
-      let questionHtml = `
-        <fieldset>
-          <legend>Question ${index + 1}</legend>
-          <p id="question${index + 1}" class="questions">${question.question}</p>
-          <label><input type="radio" name="answer${index + 1}" value="true"> True</label>
-          <label><input type="radio" name="answer${index + 1}" value="false"> False</label>
-        </fieldset>
-      `;
+                        <button class="submit" type="button">
+                            Check your answers
+                         </button>
+                        </form>
+                        <h2 id="results-h">Results</h2>
+                        <p id="results"></p>
+                </div>
+            </main>
+    </body>
+</html>
   
       quizForm.innerHTML += questionHtml;
 
